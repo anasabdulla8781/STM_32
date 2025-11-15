5a59256005d2e6690126727d02d2b0a40779fc63
Anas abdulla <anasabdulla8781@gmail.com>
2025-11-15 18:23:04 +0530

    GPIO mode additions in the function

--- Codes/10_2LED_with_differant_delays/Debug/App/src/app_main.d
+++ Codes/10_2LED_with_differant_delays/Debug/App/src/app_main.d
@@ -1,11 +1,11 @@
 App/src/app_main.o: ../App/src/app_main.c \
- D:/Anas_Embedded\ system/STM32/Codes/My_workspace/Target/10_2LED_with_differant_delays/App/inc/app_main.h \
- D:/Anas_Embedded\ system/STM32/Codes/My_workspace/Target/10_2LED_with_differant_delays/Services/inc/STM32_GPT.h \
- D:/Anas_Embedded\ system/STM32/Codes/My_workspace/Target/10_2LED_with_differant_delays/Services/inc/STM32_RCC.h \
- D:/Anas_Embedded\ system/STM32/Codes/My_workspace/Target/10_2LED_with_differant_delays/Services/inc/STM32_GPIO.h \
- D:/Anas_Embedded\ system/STM32/Codes/My_workspace/Target/10_2LED_with_differant_delays/Services/inc/STM32_NVIC.h
-D:/Anas_Embedded\ system/STM32/Codes/My_workspace/Target/10_2LED_with_differant_delays/App/inc/app_main.h:
-D:/Anas_Embedded\ system/STM32/Codes/My_workspace/Target/10_2LED_with_differant_delays/Services/inc/STM32_GPT.h:
-D:/Anas_Embedded\ system/STM32/Codes/My_workspace/Target/10_2LED_with_differant_delays/Services/inc/STM32_RCC.h:
-D:/Anas_Embedded\ system/STM32/Codes/My_workspace/Target/10_2LED_with_differant_delays/Services/inc/STM32_GPIO.h:
-D:/Anas_Embedded\ system/STM32/Codes/My_workspace/Target/10_2LED_with_differant_delays/Services/inc/STM32_NVIC.h:
+ D:/Anas_Embedded\ system/STM32_Activities/Codes/10_2LED_with_differant_delays/App/inc/app_main.h \
+ D:/Anas_Embedded\ system/STM32_Activities/Codes/10_2LED_with_differant_delays/Services/inc/STM32_GPT.h \
+ D:/Anas_Embedded\ system/STM32_Activities/Codes/10_2LED_with_differant_delays/Services/inc/STM32_RCC.h \
+ D:/Anas_Embedded\ system/STM32_Activities/Codes/10_2LED_with_differant_delays/Services/inc/STM32_GPIO.h \
+ D:/Anas_Embedded\ system/STM32_Activities/Codes/10_2LED_with_differant_delays/Services/inc/STM32_NVIC.h
+D:/Anas_Embedded\ system/STM32_Activities/Codes/10_2LED_with_differant_delays/App/inc/app_main.h:
+D:/Anas_Embedded\ system/STM32_Activities/Codes/10_2LED_with_differant_delays/Services/inc/STM32_GPT.h:
+D:/Anas_Embedded\ system/STM32_Activities/Codes/10_2LED_with_differant_delays/Services/inc/STM32_RCC.h:
+D:/Anas_Embedded\ system/STM32_Activities/Codes/10_2LED_with_differant_delays/Services/inc/STM32_GPIO.h:
+D:/Anas_Embedded\ system/STM32_Activities/Codes/10_2LED_with_differant_delays/Services/inc/STM32_NVIC.h:
--- Codes/10_2LED_with_differant_delays/Debug/Services/src/STM32_GPT.d
+++ Codes/10_2LED_with_differant_delays/Debug/Services/src/STM32_GPT.d
@@ -1,3 +1,3 @@
 Services/src/STM32_GPT.o: ../Services/src/STM32_GPT.c \
- D:/Anas_Embedded\ system/STM32/Codes/My_workspace/Target/10_2LED_with_differant_delays/Services/inc/STM32_GPT.h
-D:/Anas_Embedded\ system/STM32/Codes/My_workspace/Target/10_2LED_with_differant_delays/Services/inc/STM32_GPT.h:
+ D:/Anas_Embedded\ system/STM32_Activities/Codes/10_2LED_with_differant_delays/Services/inc/STM32_GPT.h
+D:/Anas_Embedded\ system/STM32_Activities/Codes/10_2LED_with_differant_delays/Services/inc/STM32_GPT.h:
